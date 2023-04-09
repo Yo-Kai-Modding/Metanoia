0f338de380bba11b135f686349aef2effa51e9ae
YKW Modding <115092262+Yo-Kai-Modding@users.noreply.github.com>
2023-04-09 15:19:10 +0100

    Fix Huffman Decompression
    This will make more models work properly, like map models.

--- Metanoia/Tools/Decompress.cs
+++ Metanoia/Tools/Decompress.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Linq;
+using System.Text;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
@@ -41,10 +43,10 @@ namespace Metanoia.Tools
                     t = (lzss_Decompress(b));
                     break;
                 case 0x02:
-                    t = (Huffman_Decompress(b, (byte)0x24));
+                    t = Huffman_Decompress(b, 4);
                     break;
                 case 0x03:
-                    t = (Huffman_Decompress(b, (byte)0x28));
+                    t = Huffman_Decompress(b, 8);
                     break;
                 case 0x04:
                     t = (rle_Decompress(b));
@@ -497,171 +499,109 @@ namespace Metanoia.Tools
             return outstream.ToArray();
         }
 
-        public class HUFF_STREAM
+        // HuffmanDecoder & HuffmanHeaderlessDecoder From Kuriimu2
+        public class HuffmanDecoder
         {
-            public byte[] bytes;
-            public int p = 0;
-            public int length;
-            public HUFF_STREAM(byte[] b)
+            private readonly int _bitDepth;
+            private readonly HuffmanHeaderlessDecoder _decoder;
+
+            public HuffmanDecoder(int bitDepth, NibbleOrder nibbleOrder)
             {
-                bytes = b;
-                length = b.Length;
+                _bitDepth = bitDepth;
+
+                _decoder = new HuffmanHeaderlessDecoder(bitDepth, nibbleOrder);
             }
 
-            public bool hasBytes()
+            public void Decode(Stream input, Stream output)
             {
-                return p < bytes.Length;
+                var compressionHeader = new byte[4];
+                input.Read(compressionHeader, 0, 4);
+
+                var huffmanMode = _bitDepth == 4 ? 2 : 3;
+                if ((compressionHeader[0] & 0x7) != huffmanMode)
+                    throw new InvalidDataException($"Level5 Huffman{_bitDepth}");
+
+                var decompressedSize = (compressionHeader[0] >> 3) | (compressionHeader[1] << 5) |
+                                       (compressionHeader[2] << 13) | (compressionHeader[3] << 21);
+
+                _decoder.Decode(input, output, decompressedSize);
             }
 
-            public int ReadByte()
+            public void Dispose()
             {
-                return bytes[p++] & 0xFF;
-            }
-            public int readThree()
-            {
-                return ((bytes[p++] & 0xFF)) | ((bytes[p++] & 0xFF) << 8) | ((bytes[p++] & 0xFF) << 16);
-            }
-            public int ReadInt32()
-            {
-                if (p >= length)
-                    return 0;
-                else
-                    return ((bytes[p++] & 0xFF)) | ((bytes[p++] & 0xFF) << 8) | ((bytes[p++] & 0xFF) << 16) | ((bytes[p++] & 0xFF) << 24);
+                // nothing to dispose
             }
         }
 
-        public class HuffTreeNode
+        public class HuffmanHeaderlessDecoder
         {
-            public byte data;
-            public bool isData;
-            public HuffTreeNode child0; public HuffTreeNode child1;
-            public HuffTreeNode(HUFF_STREAM stream, bool isData, long relOffset, long maxStreamPos)
+            private readonly int _bitDepth;
+            private readonly NibbleOrder _nibbleOrder;
+
+            public HuffmanHeaderlessDecoder(int bitDepth, NibbleOrder nibbleOrder)
             {
-                if (stream.p >= maxStreamPos)
-                {
-                    return;
-                }
-                int readData = stream.ReadByte();
-                this.data = (byte)readData;
-
-                this.isData = isData;
-
-                if (!this.isData)
-                {
-                    int offset = this.data & 0x3F;
-                    bool zeroIsData = (this.data & 0x80) > 0;
-                    bool oneIsData = (this.data & 0x40) > 0;
-
-                    long zeroRelOffset = (relOffset ^ (relOffset & 1)) + (offset * 2) + 2;
-
-                    int currStreamPos = stream.p;
-                    stream.p += (int)(zeroRelOffset - relOffset) - 1;
-                    this.child0 = new HuffTreeNode(stream, zeroIsData, zeroRelOffset, maxStreamPos);
-                    this.child1 = new HuffTreeNode(stream, oneIsData, zeroRelOffset + 1, maxStreamPos);
-
-                    stream.p = currStreamPos;
-                }
+                _bitDepth = bitDepth;
+                _nibbleOrder = nibbleOrder;
             }
 
+            public void Decode(Stream input, Stream output, int decompressedSize)
+            {
+                var result = new byte[decompressedSize * 8 / _bitDepth];
+
+                using (var br = new BinaryReader(input, Encoding.ASCII, true))
+                {
+                    var treeSize = br.ReadByte();
+                    var treeRoot = br.ReadByte();
+                    var treeBuffer = br.ReadBytes(treeSize * 2);
+
+                    for (int i = 0, code = 0, next = 0, pos = treeRoot, resultPos = 0; resultPos < result.Length; i++)
+                    {
+                        if (i % 32 == 0)
+                            code = br.ReadInt32();
+
+                        next += ((pos & 0x3F) << 1) + 2;
+                        var direction = (code >> (31 - i)) % 2 == 0 ? 2 : 1;
+                        var leaf = (pos >> 5 >> direction) % 2 != 0;
+
+                        pos = treeBuffer[next - direction];
+                        if (leaf)
+                        {
+                            result[resultPos++] = (byte)pos;
+                            pos = treeRoot;
+                            next = 0;
+                        }
+                    }
+                }
+
+                if (_bitDepth == 8)
+                    output.Write(result, 0, result.Length);
+                else
+                {
+                    var combinedData = _nibbleOrder == NibbleOrder.LowNibbleFirst ?
+                        Enumerable.Range(0, decompressedSize).Select(j => (byte)(result[2 * j] | (result[2 * j + 1] << 4))).ToArray() :
+                        Enumerable.Range(0, decompressedSize).Select(j => (byte)((result[2 * j] << 4) | result[2 * j + 1])).ToArray();
+
+                    output.Write(combinedData, 0, combinedData.Length);
+                }
+            }
+        }
+
+        public enum NibbleOrder
+        {
+            LowNibbleFirst,
+            HighNibbleFirst
         }
 
         public static byte[] Huffman_Decompress(byte[] b, byte atype)
         {
-            HUFF_STREAM instream = new HUFF_STREAM(b);
-            long ReadBytes = 0;
-
-            byte type = (byte)instream.ReadByte();
-            type = atype;
-            if (type != 0x28 && type != 0x24) return b;
-            int decompressedSize = instream.readThree();
-            ReadBytes += 4;
-            if (decompressedSize == 0)
+            using (var input = new MemoryStream(b))
+            using (var output = new MemoryStream())
             {
-                instream.p -= 3;
-                decompressedSize = instream.ReadInt32();
-                ReadBytes += 4;
+                var decoder = new HuffmanDecoder(atype, NibbleOrder.LowNibbleFirst);
+                decoder.Decode(input, output);
+
+                return output.ToArray();
             }
-
-            List<byte> o = new List<byte>();
-
-            int treeSize = instream.ReadByte(); ReadBytes++;
-            treeSize = (treeSize + 1) * 2;
-
-            long treeEnd = (instream.p - 1) + treeSize;
-
-            // the relative offset may be 4 more (when the initial decompressed size is 0), but
-            // since it's relative that doesn't matter, especially when it only matters if
-            // the given value is odd or even.
-            HuffTreeNode rootNode = new HuffTreeNode(instream, false, 5, treeEnd);
-
-            ReadBytes += treeSize;
-            // re-position the stream after the tree (the stream is currently positioned after the root
-            // node, which is located at the start of the tree definition)
-            instream.p = (int)treeEnd;
-
-            // the current u32 we are reading bits from.
-            int data = 0;
-            // the amount of bits left to read from <data>
-            byte bitsLeft = 0;
-
-            // a cache used for writing when the block size is four bits
-            int cachedByte = -1;
-
-            // the current output size
-            HuffTreeNode currentNode = rootNode;
-
-            while (instream.hasBytes())
-            {
-                while (!currentNode.isData)
-                {
-                    // if there are no bits left to read in the data, get a new byte from the input
-                    if (bitsLeft == 0)
-                    {
-                        ReadBytes += 4;
-                        data = instream.ReadInt32();
-                        bitsLeft = 32;
-                    }
-                    // get the next bit
-                    bitsLeft--;
-                    bool nextIsOne = (data & (1 << bitsLeft)) != 0;
-                    // go to the next node, the direction of the child depending on the value of the current/next bit
-                    currentNode = nextIsOne ? currentNode.child1 : currentNode.child0;
-                }
-
-                switch (type)
-                {
-                    case 0x28:
-                        {
-                            // just copy the data if the block size is a full byte
-                            //                        outstream.WriteByte(currentNode.Data);
-                            o.Add(currentNode.data);
-                            break;
-                        }
-                    case 0x24:
-                        {
-                            // cache the first half of the data if the block size is a half byte
-                            if (cachedByte < 0)
-                            {
-                                cachedByte = currentNode.data;
-                            }
-                            else
-                            {
-                                cachedByte |= currentNode.data << 4;
-                                o.Add((byte)cachedByte);
-                                cachedByte = -1;
-                            }
-                            break;
-                        }
-                }
-
-                currentNode = rootNode;
-            }
-
-            if (ReadBytes % 4 != 0)
-                ReadBytes += 4 - (ReadBytes % 4);
-
-
-            return o.ToArray();
         }
 
         public static byte[] ZLIB(byte[] data)
